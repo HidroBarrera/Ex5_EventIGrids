9e5363dcbab51a2b4d940f7b04f393194b6bf2ca
Jordi <14jbarrera@gmail.com>
2023-10-09 16:59:39 +0200

    Feta

--- Ex5/MainWindow.xaml.cs
+++ Ex5/MainWindow.xaml.cs
@@ -24,5 +24,105 @@ namespace Ex5
         {
             InitializeComponent();
         }
+
+        private void Button_Click_00(object sender, RoutedEventArgs e)
+        {
+            if (B00.Content.ToString() == "" || B00.Content.ToString() == "0")
+            {
+                B00.Content = "X";
+            }
+            else
+            {
+                B00.Content = "0";
+            }
+        }
+        private void Button_Click_01(object sender, RoutedEventArgs e)
+        {
+            if (B01.Content.ToString() == "" || B01.Content.ToString() == "0")
+            {
+                B01.Content = "X";
+            }
+            else
+            {
+                B01.Content = "0";
+            }
+        }
+        private void Button_Click_02(object sender, RoutedEventArgs e)
+        {
+            if (B02.Content.ToString() == "" || B02.Content.ToString() == "0")
+            {
+                B02.Content = "X";
+            }
+            else
+            {
+                B02.Content = "0";
+            }
+        }
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            if (B10.Content.ToString() == "" || B10.Content.ToString() == "0")
+            {
+                B10.Content = "X";
+            }
+            else
+            {
+                B10.Content = "0";
+            }
+        }
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            if (B11.Content.ToString() == "" || B11.Content.ToString() == "0")
+            {
+                B11.Content = "X";
+            }
+            else
+            {
+                B11.Content = "0";
+            }
+        }
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            if (B12.Content.ToString() == "" || B12.Content.ToString() == "0")
+            {
+                B12.Content = "X";
+            }
+            else
+            {
+                B12.Content = "0";
+            }
+        }
+        private void Button_Click_20(object sender, RoutedEventArgs e)
+        {
+            if (B20.Content.ToString() == "" || B20.Content.ToString() == "0")
+            {
+                B20.Content = "X";
+            }
+            else
+            {
+                B20.Content = "0";
+            }
+        }
+        private void Button_Click_21(object sender, RoutedEventArgs e)
+        {
+            if (B21.Content.ToString() == "" || B21.Content.ToString() == "0")
+            {
+                B21.Content = "X";
+            }
+            else
+            {
+                B21.Content = "0";
+            }
+        }
+        private void Button_Click_22(object sender, RoutedEventArgs e)
+        {
+            if (B22.Content.ToString() == "" || B22.Content.ToString() == "0")
+            {
+                B22.Content = "X";
+            }
+            else
+            {
+                B22.Content = "0";
+            }
+        }
     }
 }
